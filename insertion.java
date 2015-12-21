877b272a61ece25ce640d252d34fa6176e1c106b
Karin Touchie <ktouchie@gmail.com>
2015-12-21 11:53:37 +0100

    Move files from other directory

---
+++ insertion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class insertion {
+
+	public static void main(String[] args) {
+        
+        int[] tab = {5,9,4,1,2,7,3,8,6,0};
+        
+        boolean swap = false;
+        
+        do {
+            swap = false;
+            for (int i=tab.length-1; i>0; i--) {
+                int x = tab[i];
+                int j = i;
+                while (j>0 && x<tab[j-1]) {
+                    tab[j] = tab[j-1];
+                    j = j-1;
+                    swap=true;
+                }
+                tab[j] = x;
+            }
+        } while (swap==true);
+        
+        System.out.println(Arrays.toString(tab));
+    }
+}
